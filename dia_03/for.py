67ced75807459b9d826d05baaead8c95c0a15d2f
Mateus Alsina <mateusalsinacarvalho@gmail.com>
2025-07-30 18:25:30 -0300

    aprendendo o laço for

--- dia_03/for.py
+++ dia_03/for.py
@@ -3,3 +3,17 @@ nome = "mateus"
 for letra in nome:
     print(letra)
     
+#o for percorre cada letra da string nome e imprime uma por uma
+#o for é uma estrutura de repetição que itera sobre os elementos de uma coleção, 
+#como uma string, lista ou tupla, executando um bloco de código para cada elemento.
+#nesse caso, o for percorre cada letra da string nome e imprime uma por uma
+
+#%% 
+
+numero = 2 
+max_numero = 100    
+
+for i in range(1 , max_numero+1):
+        print(numero, "X", i, "=", max_numero * i)
+
+    
