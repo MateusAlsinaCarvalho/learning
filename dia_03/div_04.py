bc3e2263720736fd7a1c8b3269261e315620f15a
Mateus Alsina <mateusalsinacarvalho@gmail.com>
2025-07-31 14:38:33 -0300

    Aprendendo sobre listas

--- dia_03/div_04.py
+++ dia_03/div_04.py
@@ -10,3 +10,17 @@ while count <= 100:
     if resto == 0:      #se o resto for igual a 0, significa que count é divisível por 4
         print(count)    #imprime o valor de count, pois é divisível por 4
     count += 1         #incrementa o valor de count em 1 a cada iteração do laço
+
+
+#%%
+#aqui faremos o mesmo programa mas utilizando o for 
+
+for numero in range(4, 101):
+    if numero % 4 == 0:
+        print(numero)
+
+
+#O while utilizamos para comparações lógicas, repetidos por uma condição lógica
+#O for usamos para percorrer os elementos de um objeto, seja uma string ou uma sequencia 
+#normalmente usamos o While quando não sabemos quantas vezes iremos repetir, o for sabemos 
+#qual objeto queremos percorrer. 
---
+++ dia_04/listas.py
@@ -0,0 +1,59 @@
+#as listas são um conjunto de elementos 
+#sempre dentro das listas usando colchetes
+#uma das maneiras de definir listas:
+#uma coisa importante sobre listas: listas no python não são arrays
+#em python podemos ter listas de absolutamente qualquer tipo de elementos
+#e esses elementos não precisam ser do mesmo tipo.
+#As listas são análogas a um molho de chaves
+
+#%%
+idades = [28, 42, 43, 35, 39, 28, 38]
+print(idades)
+
+
+#%%
+mateus = ["Mateus", "Alsina", 26, False, "Solteiro", 1800.50]
+print(mateus) 
+
+
+#%% 
+type(mateus)
+
+#%%
+#para acessar as posições dentro da lista fazemos:
+#para apontar um elemento de uma lista eu preciso apontar o indice 
+#desse elemento. Os elementos dentro de uma lista começam no zero.
+
+#nome do elemento
+print(mateus[0])
+
+#idade do Mateus 
+print(mateus[2])
+
+#estado civil do Mateus
+print(mateus[4])
+
+#%%
+
+idades = [20, 89, 34, 89, 26, 19, 90, 42]
+
+print("Soma das idades: ", sum(idades))
+
+print("Quantidade de idades: ", len(idades))
+
+print("Média das idades: ", sum(idades)/len(idades))
+
+#ao realizar um hardcoding, ou seja, quando codamos de forma mais 
+#rustica ou sem usar tantas ferramentas, algo como utilizar "sum(idades)/9"
+#ao inves de utilizarmos o len, isso pode causar bugs, que não necessariamente quebram
+#o código causando um erro, mas pode apontar conclusões erradas.
+#Se por exemplo, esquecermos de atualizar o numero "9" quando inserimos
+#um elemento a mais na lista, a média será exibida de forma erronea. Isso 
+#não irá causar um erro, mas o programa funcionará de uma forma que não era a esperada
+#Não é uma regra, mas utilizar menos códigos "chumbados" ou menos hardcoding pode te 
+#fazer um programador melhor.
+
+print("Menor idade: ", min(idades)) 
+
+print("Maior idade: ", max(idades))
+
